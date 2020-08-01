12ee139ffce8c967eb9b61be725683055ae604f0
joselo10miguel <56126414+joselo10miguel@users.noreply.github.com>
2020-07-31 19:11:41 -0500

    Add files via upload

--- proyecto_poo/Ingreso_de_Productos_alas_Perchas.java
+++ proyecto_poo/Ingreso_de_Productos_alas_Perchas.java
@@ -240,13 +240,13 @@ public class Ingreso_de_Productos_alas_Perchas extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Ingreso_PUtencillos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ingreso_de_Productos_alas_Perchas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Ingreso_PUtencillos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ingreso_de_Productos_alas_Perchas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Ingreso_PUtencillos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ingreso_de_Productos_alas_Perchas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Ingreso_PUtencillos.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(Ingreso_de_Productos_alas_Perchas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
